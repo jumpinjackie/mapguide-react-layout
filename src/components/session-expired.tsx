8bf6e2a92719ef6087916e8a195c78fefa773e5f
Jackie Ng <jumpinjackie@gmail.com>
2016-09-05 02:12:24 +1000

    #26: Add reloading the viewer as an available action

--- src/components/session-expired.tsx
+++ src/components/session-expired.tsx
@@ -1,7 +1,22 @@
 import * as React from "react";
 
+function reload(e) {
+    e.preventDefault();
+    //TODO: This is obviosly the nuclear solution.
+    //
+    //The more graceful solution is to re-create the runtime map and interleave the response
+    //into the current redux store and preserve the existing view and layer/group state and
+    //we should be able to continue where we left off
+    window.location.reload();
+    return false;
+}
+
 export const SessionExpired = (props) => {
     return <div style={{ padding: 5 }}>
         <p>Your current MapGuide session has expired.</p>
+        <p><strong>Available Actions:</strong></p>
+        <ul>
+            <li><a href="#" onClick={reload}>Reload the viewer</a></li>
+        </ul>
     </div>;
 };

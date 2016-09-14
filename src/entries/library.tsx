2bb042c57959af1d473e77ee2fa3145454edf154
Jackie Ng <jumpinjackie@gmail.com>
2016-09-14 22:55:14 +1000

    - Remove isomorphic-fetch (we're using whatwg-fetch)
    - Pin specific OpenLayers 3 version

--- src/entries/library.tsx
+++ src/entries/library.tsx
@@ -13,7 +13,7 @@ import "../styles/index.css";
 import * as ol from "openlayers"; 
 const proj4 = require("proj4");
 require("es6-promise").polyfill();
-require('isomorphic-fetch');
+require('whatwg-fetch');
 ol.proj.setProj4(proj4);
 
 registerLayout("ajax-viewer", () => <AjaxViewerLayout />);

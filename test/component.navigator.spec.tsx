4604a7bcc4d01782b381f488ab7f48b5ffa9dbd7
Jackie Ng <jumpinjackie@gmail.com>
2016-09-15 01:49:39 +1000

    Add click handler verification tests for Navigator

--- test/component.navigator.spec.tsx
+++ test/component.navigator.spec.tsx
@@ -1,24 +1,75 @@
 import * as React from "react";
 import { expect } from "chai";
 import { shallow, mount, render } from "enzyme";
-import { Navigator } from "../src/components/navigator";
+import { Navigator, PanDirection, ZoomDirection } from "../src/components/navigator";
 import td = require("testdouble");
+
+type PanFunc = (direction: PanDirection) => void;
+type ZoomFunc = (direction: ZoomDirection) => void;
 
 describe("components/navigator", () => {
     it("renders spinner shown when busy", () => {
-        const onPan = td.function<any>();
+        const onPan = td.function<PanFunc>();
         const onRequestZoomToScale = td.function<any>();
-        const onZoom = td.function<any>();
+        const onZoom = td.function<ZoomFunc>();
         const wrapper = shallow(<Navigator onPan={onPan} onRequestZoomToScale={onRequestZoomToScale} onZoom={onZoom} busy={true} scale={5000} />);
         expect(wrapper.find("img.navigator-spinner")).to.have.length(1);
         expect((wrapper.find("img.navigator-spinner").props() as any).style.visibility).to.be.equal("visible");
     });
     it("renders spinner hidden when not busy", () => {
-        const onPan = td.function<any>();
+        const onPan = td.function<PanFunc>();
         const onRequestZoomToScale = td.function<any>();
-        const onZoom = td.function<any>();
+        const onZoom = td.function<ZoomFunc>();
         const wrapper = shallow(<Navigator onPan={onPan} onRequestZoomToScale={onRequestZoomToScale} onZoom={onZoom} busy={false} scale={5000} />);
         expect(wrapper.find("img.navigator-spinner")).to.have.length(1);
         expect((wrapper.find("img.navigator-spinner").props() as any).style.visibility).to.be.equal("hidden");
+    });
+    it("clicking right raises pan east", () => {
+        const onPan = td.function<PanFunc>();
+        const onRequestZoomToScale = td.function<any>();
+        const onZoom = td.function<ZoomFunc>();
+        const wrapper = shallow(<Navigator onPan={onPan} onRequestZoomToScale={onRequestZoomToScale} onZoom={onZoom} busy={false} scale={5000} />);
+        wrapper.find("area").at(0).simulate("click");
+        td.verify(onPan(PanDirection.East));
+    });
+    it("clicking left raises pan west", () => {
+        const onPan = td.function<PanFunc>();
+        const onRequestZoomToScale = td.function<any>();
+        const onZoom = td.function<ZoomFunc>();
+        const wrapper = shallow(<Navigator onPan={onPan} onRequestZoomToScale={onRequestZoomToScale} onZoom={onZoom} busy={false} scale={5000} />);
+        wrapper.find("area").at(1).simulate("click");
+        td.verify(onPan(PanDirection.West));
+    });
+    it("clicking down raises pan south", () => {
+        const onPan = td.function<PanFunc>();
+        const onRequestZoomToScale = td.function<any>();
+        const onZoom = td.function<ZoomFunc>();
+        const wrapper = shallow(<Navigator onPan={onPan} onRequestZoomToScale={onRequestZoomToScale} onZoom={onZoom} busy={false} scale={5000} />);
+        wrapper.find("area").at(2).simulate("click");
+        td.verify(onPan(PanDirection.South));
+    });
+    it("clicking up raises pan north", () => {
+        const onPan = td.function<PanFunc>();
+        const onRequestZoomToScale = td.function<any>();
+        const onZoom = td.function<ZoomFunc>();
+        const wrapper = shallow(<Navigator onPan={onPan} onRequestZoomToScale={onRequestZoomToScale} onZoom={onZoom} busy={false} scale={5000} />);
+        wrapper.find("area").at(3).simulate("click");
+        td.verify(onPan(PanDirection.North));
+    });
+    it("clicking - raises zoom out", () => {
+        const onPan = td.function<PanFunc>();
+        const onRequestZoomToScale = td.function<any>();
+        const onZoom = td.function<ZoomFunc>();
+        const wrapper = shallow(<Navigator onPan={onPan} onRequestZoomToScale={onRequestZoomToScale} onZoom={onZoom} busy={false} scale={5000} />);
+        wrapper.find("area").at(4).simulate("click");
+        td.verify(onZoom(ZoomDirection.Out));
+    });
+    it("clicking + raises zoom in", () => {
+        const onPan = td.function<PanFunc>();
+        const onRequestZoomToScale = td.function<any>();
+        const onZoom = td.function<ZoomFunc>();
+        const wrapper = shallow(<Navigator onPan={onPan} onRequestZoomToScale={onRequestZoomToScale} onZoom={onZoom} busy={false} scale={5000} />);
+        wrapper.find("area").at(5).simulate("click");
+        td.verify(onZoom(ZoomDirection.In));
     });
 });

59677420948d59f860fde574b9f345d516fa3975
Mikhael Milikhin <mikhael@milikhin.name>
2017-11-27 22:39:11 +0700

    Update demo.sh

--- demo.sh
+++ demo.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-cd docker/demo
-docker build -t jumpinjackie/mapguide-react-layout-demo .
+(cd docker/demo; docker build -t mapguide-react .)
 # run without starting tomcat
-docker run -p 80:8008 -t jumpinjackie/mapguide-react-layout-demo --no-tomcat
+docker run -p 80:8008 \
+  --mount type=bind,source="$(pwd)"/viewer,target=/usr/local/mapguideopensource-3.1.0/webserverextensions/www/viewer \
+  -t mapguide-react --no-tomcat

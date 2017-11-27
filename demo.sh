3b67b59b713f417c2a46091aa47fd83671af502f
mikhael <mikhael@milikhin.name>
2017-11-27 23:02:21 +0700

    initial hackathon setup

--- demo.sh
+++ demo.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-(cd docker/demo; docker build -t mapguide-react .)
-# run without starting tomcat
+(cd docker/demo; docker build -t mapguide-react .) && \
 docker run -p 80:8008 \
   --mount type=bind,source="$(pwd)"/viewer,target=/usr/local/mapguideopensource-3.1.0/webserverextensions/www/viewer \
   -t mapguide-react --no-tomcat

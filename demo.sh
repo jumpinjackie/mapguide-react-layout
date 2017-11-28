ddad768bf4480c22c1e5d1ee93ffb6a83a4ae5f2
mikhael <mikhael@milikhin.name>
2017-11-29 00:30:49 +0700

    add postgres

--- demo.sh
+++ demo.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 (cd docker/demo; docker build -t mapguide-react .) && \
-docker run -p 80:8008 --rm \
-  --mount type=bind,source="$(pwd)"/viewer,target=/usr/local/mapguideopensource-3.1.0/webserverextensions/www/viewer \
-  --mount type=bind,source="$(pwd)"/fusion,target=/usr/local/mapguideopensource-3.1.0/webserverextensions/www/fusion \
-  -t mapguide-react --no-tomcat
+#docker run -p 80:8008 --rm \
+#  --mount type=bind,source="$(pwd)"/viewer,target=/usr/local/mapguideopensource-3.1.0/webserverextensions/www/viewer \
+#  --mount type=bind,source="$(pwd)"/fusion,target=/usr/local/mapguideopensource-3.1.0/webserverextensions/www/fusion \
+#  -t mapguide-react --no-tomcat
+docker stack deploy -c docker/demo/docker-compose.yml webgis && docker service ls

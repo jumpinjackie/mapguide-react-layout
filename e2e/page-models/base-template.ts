08ac000ea7294cb1ace5c8ad78cdf179e77c998d
Jackie Ng <jumpinjackie@gmail.com>
2018-07-12 02:28:00 +1000

    #643: Query by react component name

--- e2e/page-models/base-template.ts
+++ e2e/page-models/base-template.ts
@@ -4,7 +4,7 @@ import { ReactSelector } from "testcafe-react-selectors";
 export abstract class BaseTemplate {
     private map: Selector;
     constructor(protected test: TestController) {
-        this.map = Selector(".map-viewer-component").with({ visibilityCheck: true });
+        this.map = ReactSelector("MapViewerBase");
     }
     public async assertMapPresent() {
         await this.test.expect(this.map.exists).ok();

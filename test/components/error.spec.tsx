a641db24f319aacac81df1963bcfb5f2abdb0c90
Jackie Ng <jumpinjackie@gmail.com>
2019-11-08 00:46:53 +1100

    Fix failing tests due to obsolete css class references

--- test/components/error.spec.tsx
+++ test/components/error.spec.tsx
@@ -21,17 +21,17 @@ describe("components/error", () => {
     it("renders a MgError with stack", () => {
         const err = captureError();
         const wrapper = shallow(<Error error={err} />);
-        expect(wrapper.find(".pt-callout")).toHaveLength(1);
-        expect(wrapper.find(".pt-callout .error-header")).toHaveLength(1);
-        expect(wrapper.find(".pt-callout .error-header").text()).toBe(err.message);
-        expect(wrapper.find(".pt-callout .error-stack")).toHaveLength(1);
+        expect(wrapper.find(".bp3-callout")).toHaveLength(1);
+        expect(wrapper.find(".bp3-callout .error-header")).toHaveLength(1);
+        expect(wrapper.find(".bp3-callout .error-header").text()).toBe(err.message);
+        expect(wrapper.find(".bp3-callout .error-stack")).toHaveLength(1);
     });
     it("renders a string without a stack", () => {
         const err = "Uh oh!";
         const wrapper = shallow(<Error error={err} />);
-        expect(wrapper.find(".pt-callout")).toHaveLength(1);
-        expect(wrapper.find(".pt-callout .error-header")).toHaveLength(1);
-        expect(wrapper.find(".pt-callout .error-header").text()).toBe(err);
-        expect(wrapper.find(".pt-callout .error-stack")).toHaveLength(0);
+        expect(wrapper.find(".bp3-callout")).toHaveLength(1);
+        expect(wrapper.find(".bp3-callout .error-header")).toHaveLength(1);
+        expect(wrapper.find(".bp3-callout .error-header").text()).toBe(err);
+        expect(wrapper.find(".bp3-callout .error-stack")).toHaveLength(0);
     });
 });

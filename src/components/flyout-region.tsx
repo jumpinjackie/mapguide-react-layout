bf4df85229c7539555645b1a51c8af77cfcf4c1a
Jackie Ng <jumpinjackie@gmail.com>
2019-11-21 23:16:03 +1100

    Hook-ify FlyoutRegion

--- src/components/flyout-region.tsx
+++ src/components/flyout-region.tsx
@@ -36,78 +36,69 @@ export interface IFlyoutRegionProps {
  * @class FlyoutRegion
  * @extends {React.Component<IFlyoutRegionProps, any>}
  */
-export class FlyoutRegion extends React.Component<IFlyoutRegionProps, any> {
-    constructor(props: IFlyoutRegionProps) {
-        super(props);
-        this.state = {};
-    }
-    private onChildInvoked(id: string) {
-        this.props.onCloseFlyout(id);
-    }
-    render(): JSX.Element {
-        return <div>
-            {(() => {
-                const children = [] as JSX.Element[];
-                for (const flyoutId in this.props.flyoutConf) {
-                    const flyout = this.props.flyoutConf[flyoutId];
-                    const open = !!flyout.open;
-                    if (open) {
-                        const items = flyout.childItems || [];
-                        const containerStyle: React.CSSProperties = {};
-                        containerStyle.zIndex = 2000; //This should be big enough to be above all possible UI elements
-                        if (flyout.metrics) {
-                            const met = flyout.metrics;
-                            if (flyoutId == Constants.WEBLAYOUT_CONTEXTMENU) {
-                                // TODO: Refine the layout positioning so that it better behaves like it did
-                                // previously when the blueprint context menu worked (ie. If right-clicking near
-                                // the bottom, the menu should be shown "above" the cursor)
-                                containerStyle.top = met.posY - 40;
-                                containerStyle.left = met.posX + 20;
+export const FlyoutRegion = (props: IFlyoutRegionProps) => {
+    return <div>
+        {(() => {
+            const children = [] as JSX.Element[];
+            for (const flyoutId in props.flyoutConf) {
+                const flyout = props.flyoutConf[flyoutId];
+                const open = !!flyout.open;
+                if (open) {
+                    const items = flyout.childItems || [];
+                    const containerStyle: React.CSSProperties = {};
+                    containerStyle.zIndex = 2000; //This should be big enough to be above all possible UI elements
+                    if (flyout.metrics) {
+                        const met = flyout.metrics;
+                        if (flyoutId == Constants.WEBLAYOUT_CONTEXTMENU) {
+                            // TODO: Refine the layout positioning so that it better behaves like it did
+                            // previously when the blueprint context menu worked (ie. If right-clicking near
+                            // the bottom, the menu should be shown "above" the cursor)
+                            containerStyle.top = met.posY - 40;
+                            containerStyle.left = met.posX + 20;
+                        } else {
+                            if (flyout.metrics.vertical === true) {
+                                containerStyle.top = met.posY;
                             } else {
+                                containerStyle.top = met.posY + met.height;
+                            }
+                            if (flyoutId == Constants.WEBLAYOUT_TASKMENU) {
+                                containerStyle.right = window.innerWidth - (met.posX + met.width);
+                            } else {
+                                containerStyle.left = met.posX;
                                 if (flyout.metrics.vertical === true) {
-                                    containerStyle.top = met.posY;
-                                } else {
-                                    containerStyle.top = met.posY + met.height;
-                                }
-                                if (flyoutId == Constants.WEBLAYOUT_TASKMENU) {
-                                    containerStyle.right = window.innerWidth - (met.posX + met.width);
-                                } else {
-                                    containerStyle.left = met.posX;
-                                    if (flyout.metrics.vertical === true) {
-                                        containerStyle.left += met.width;
-                                    }
+                                    containerStyle.left += met.width;
                                 }
                             }
                         }
-                        const invoked = () => {
-                            this.onChildInvoked(flyoutId);
-                        };
-                        let className = "mg-flyout-menu-container";
-                        if (flyout.componentName) {
-                            className = "mg-flyout-component-container";
-                        }
-
-                        children.push(<div key={flyoutId} className={className} style={containerStyle}>
-                            {(() => {
-                                if (flyout.componentName) {
-                                    return <PlaceholderComponent id={flyout.componentName} componentProps={flyout.componentProps} locale={this.props.locale} />;
-                                } else {
-                                    return <MenuComponent items={items} onInvoked={invoked} />;
-                                }
-                            })()}
-                            {(() => {
-                                if (flyoutId === Constants.WEBLAYOUT_TASKMENU) {
-                                    //HACK: In order for this flyout to show properly over the task pane iframe
-                                    //when it contains embedded content (eg. An ActiveX/Flash/etc control) in IE
-                                    //we have to stick an iframe into this flyout
-                                    return <iframe src="about:blank" className="iframe-iehack-zindex" />;
-                                }
-                            })()}
-                        </div>);
                     }
+                    const invoked = () => {
+                        props.onCloseFlyout(id);
+                    };
+                    let className = "mg-flyout-menu-container";
+                    if (flyout.componentName) {
+                        className = "mg-flyout-component-container";
+                    }
+
+                    children.push(<div key={flyoutId} className={className} style={containerStyle}>
+                        {(() => {
+                            if (flyout.componentName) {
+                                return <PlaceholderComponent id={flyout.componentName} componentProps={flyout.componentProps} locale={props.locale} />;
+                            } else {
+                                return <MenuComponent items={items} onInvoked={invoked} />;
+                            }
+                        })()}
+                        {(() => {
+                            if (flyoutId === Constants.WEBLAYOUT_TASKMENU) {
+                                //HACK: In order for this flyout to show properly over the task pane iframe
+                                //when it contains embedded content (eg. An ActiveX/Flash/etc control) in IE
+                                //we have to stick an iframe into this flyout
+                                return <iframe src="about:blank" className="iframe-iehack-zindex" />;
+                            }
+                        })()}
+                    </div>);
                 }
-                return children;
-            })()}
-        </div>;
-    }
+            }
+            return children;
+        })()}
+    </div>;
 }

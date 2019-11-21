1e1148a2122c13561ee82cf4b1116da3ed4fbeb6
Jackie Ng <jumpinjackie@gmail.com>
2019-11-22 01:29:14 +1100

    Hook-ify MapMenu, MenuComponent and Navigator. Tidy up imports for modal-dialog

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -19,58 +19,49 @@ export interface IMenuComponentProps {
 
 /**
  * A generic menu component
- *
- * @export
- * @class MenuComponent
- * @extends {React.Component<IMenuComponentProps, any>}
+ * @param props 
  */
-export class MenuComponent extends React.Component<IMenuComponentProps, any> {
-    constructor(props: IMenuComponentProps) {
-        super(props);
-    }
-    private onClick(item: IItem) {
+export const MenuComponent = (props: IMenuComponentProps) => {
+    const onClick = (item: IItem) => {
         if (getEnabled(item)) {
             item?.invoke?.();
-            this.props.onInvoked?.();
+            props.onInvoked?.();
         }
     }
-    render(): JSX.Element {
-        return <Menu className="bp3-elevation-1">
-            {this.props.items.map((item, index) => {
-                if (item.isSeparator) {
-                    return <MenuDivider key={index} />;
-                } else if (isMenu(item)) {
-                    let text = "";
-                    if (typeof(item.label) == 'function') {
-                        text = item.label();
-                    } else {
-                        text = item.label || "";
-                    }
-                    return <MenuItem key={index} text={text} icon="menu-open">
-                        <MenuComponent items={item.childItems} onInvoked={this.props.onInvoked} />
-                    </MenuItem>;
+    return <Menu className="bp3-elevation-1">
+        {props.items.map((item, index) => {
+            if (item.isSeparator) {
+                return <MenuDivider key={index} />;
+            } else if (isMenu(item)) {
+                let text = "";
+                if (typeof (item.label) == 'function') {
+                    text = item.label();
                 } else {
-                    const height = MENU_ITEM_HEIGHT;
-                    const invoke = this.onClick.bind(this, item);
-                    let enabled = true;
-                    if (item.enabled != null) {
-                        if (typeof(item.enabled) == 'function') {
-                            enabled = item.enabled();
-                        } else {
-                            enabled = item.enabled;
-                        }
-                    }
-                    const imgStyle = {
-                        marginRight: 5,
-                        ...getIconStyle(enabled, height)
-                    };
-                    const text = item.label || "";
-                    //NOTE: Not using MenuItem here as we want fine control over the item content
-                    return <li key={index}>
-                        <a className="bp3-menu-item" onClick={invoke}><Icon style={imgStyle} url={item.icon} spriteClass={item.iconClass} /> {text}</a>
-                    </li>;
+                    text = item.label || "";
                 }
-            })}
-        </Menu>;
-    }
+                return <MenuItem key={index} text={text} icon="menu-open">
+                    <MenuComponent items={item.childItems} onInvoked={props.onInvoked} />
+                </MenuItem>;
+            } else {
+                const height = MENU_ITEM_HEIGHT;
+                let enabled = true;
+                if (item.enabled != null) {
+                    if (typeof (item.enabled) == 'function') {
+                        enabled = item.enabled();
+                    } else {
+                        enabled = item.enabled;
+                    }
+                }
+                const imgStyle = {
+                    marginRight: 5,
+                    ...getIconStyle(enabled, height)
+                };
+                const text = item.label || "";
+                //NOTE: Not using MenuItem here as we want fine control over the item content
+                return <li key={index}>
+                    <a className="bp3-menu-item" onClick={() => onClick(item)}><Icon style={imgStyle} url={item.icon} spriteClass={item.iconClass} /> {text}</a>
+                </li>;
+            }
+        })}
+    </Menu>;
 }

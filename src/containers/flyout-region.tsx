eef4cf6965b473256a18ba3b45eb48bb97dde59a
Jackie Ng <jumpinjackie@gmail.com>
2019-11-28 02:00:47 +1100

    #1029: Clean up unused imports

--- src/containers/flyout-region.tsx
+++ src/containers/flyout-region.tsx
@@ -1,33 +1,26 @@
 import * as React from "react";
-import * as PropTypes from "prop-types";
-import { connect, useSelector, useDispatch } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { FlyoutRegion } from "../components/flyout-region";
 import * as FlyoutActions from "../actions/flyout";
 import * as MapActions from "../actions/map";
 import {
     ICommand,
-    ReduxDispatch,
     IApplicationState,
     PropType
 } from "../api/common";
 import { mapToolbarReference } from "../api/registry/command";
-import { DEFAULT_LOCALE } from "../api/i18n";
 
 export interface IFlyoutRegionContainerProps {
 
 }
 
-interface IFlyoutRegionContainerState {
-    flyouts: any;
-    locale: string;
-}
 
 interface FRDispatch {
     closeFlyout: (id: string) => void;
     invokeCommand: (cmd: ICommand, parameters?: any) => void;
 }
 
-const FlyoutRegionContainer = (props: IFlyoutRegionContainerProps) => {
+const FlyoutRegionContainer = () => {
     const appState = useSelector<IApplicationState, IApplicationState>(s => s);
     const dispatch = useDispatch();
     const closeFlyout: PropType<FRDispatch, "closeFlyout"> = (id) => dispatch(FlyoutActions.closeFlyout(id));

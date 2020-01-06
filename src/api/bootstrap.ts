4a348713fc2640bf6c556c6bcb38f51861a539c8
Jackie Ng <jumpinjackie@gmail.com>
2020-01-07 00:33:34 +1100

    #1066: Remove whatwg-fetch and es6-promise polyfill script. Delegate everything to polyfill.io

--- src/api/bootstrap.ts
+++ src/api/bootstrap.ts
@@ -1,14 +1,8 @@
 /**
- * Sets up key dependencies needed by the viewer:
- *
- *  - proj4js
- *  - polyfill for fetch
- *
- * You must call this function if rolling your own viewer bundle. This is automatically
- * called in the default viewer bundle
+ * Sets up key dependencies needed by the viewer
  *
  * @export
  */
 export function bootstrap() {
-    require('whatwg-fetch');
+    
 }

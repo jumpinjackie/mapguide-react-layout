c4604662a012a127a25b1ca9106e2ce35af6ca25
Jackie Ng <jumpinjackie@gmail.com>
2020-01-21 20:45:21 +1100

    Monkey-patch the handleEvent method of OL PointerInteraction to fix (https://github.com/openlayers/openlayers/issues/10232) until OL 6.1.2 or newer is available, which is when we'll remove this patch.
    Fixes #1090

--- src/api/bootstrap.ts
+++ src/api/bootstrap.ts
@@ -1,8 +1,45 @@
+import PointerInteraction from "ol/interaction/Pointer";
+import MapBrowserEventType from "ol/MapBrowserEventType";
+
 /**
  * Sets up key dependencies needed by the viewer
  *
  * @export
  */
 export function bootstrap() {
-    
+    //HACK: Monkey-patch the handleEvent method with the proper fix for this:
+    // https://github.com/openlayers/openlayers/issues/10232
+    //
+    //Remove this patch once OL 6.1.2 or newer is available
+    PointerInteraction.prototype.handleEvent = function (mapBrowserEvent: any) {
+        if (!( /** @type {import("../MapBrowserPointerEvent.js").default} */(mapBrowserEvent).pointerEvent)) {
+            return true;
+        }
+        var stopEvent = false;
+        this.updateTrackedPointers_(mapBrowserEvent);
+        if (this.handlingDownUpSequence) {
+            if (mapBrowserEvent.type == MapBrowserEventType.POINTERDRAG) {
+                this.handleDragEvent(mapBrowserEvent);
+            }
+            else if (mapBrowserEvent.type == MapBrowserEventType.POINTERUP) {
+                var handledUp = this.handleUpEvent(mapBrowserEvent);
+                this.handlingDownUpSequence = handledUp && this.targetPointers.length > 0;
+            }
+        }
+        else {
+            if (mapBrowserEvent.type == MapBrowserEventType.POINTERDOWN) {
+                var handled = this.handleDownEvent(mapBrowserEvent);
+                //=== This is the fix in question === //
+                //if (handled) {
+                //    mapBrowserEvent.preventDefault();
+                //}
+                this.handlingDownUpSequence = handled;
+                stopEvent = this.stopDown(handled);
+            }
+            else if (mapBrowserEvent.type == MapBrowserEventType.POINTERMOVE) {
+                this.handleMoveEvent(mapBrowserEvent);
+            }
+        }
+        return !stopEvent;
+    };
 }

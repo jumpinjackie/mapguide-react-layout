eb6b3fdfe8f85e2050bf65d28dd13f9d9d47bad3
Jackie Ng <jumpinjackie@gmail.com>
2020-02-13 23:55:34 +1100

    Remove OL 6.2 monkey-patch. I had an extremely old local copy of the OL stylesheet (probably circa OL 4.6.5), so it is missing the required pointer-events CSS. Including the actual OL stylesheet from the npm module makes all the mouse events works again (ref: https://github.com/openlayers/openlayers/issues/6948#issuecomment-585734954)

--- src/api/bootstrap.ts
+++ src/api/bootstrap.ts
@@ -1,43 +1,6 @@
-import PluggableMap from 'ol/PluggableMap';
-
 /**
  * Sets up key dependencies needed by the viewer
  *
  * @export
  */
-export function bootstrap() {
-
-    PluggableMap.prototype.handleMapBrowserEvent = function (mapBrowserEvent: any) {
-        if (!this.frameState_) {
-            // With no view defined, we cannot translate pixels into geographical
-            // coordinates so interactions cannot be used.
-            return;
-        }
-        var target = /** @type {Node} */ (mapBrowserEvent.originalEvent.target);
-        if (!mapBrowserEvent.dragging) {
-            // <========= Patch is here ==========> //
-            if (!this.viewport_.contains(target)) {
-                // Abort if the event target is a child of the container that doesn't allow
-                // event propagation or is no longer in the page. It's possible for the target to no longer
-                // be in the page if it has been removed in an event listener, this might happen in a Control
-                // that recreates it's content based on user interaction either manually or via a render
-                // in something like https://reactjs.org/
-                return;
-            }
-        }
-        mapBrowserEvent.frameState = this.frameState_;
-        var interactionsArray = this.getInteractions().getArray();
-        if (this.dispatchEvent(mapBrowserEvent) !== false) {
-            for (var i = interactionsArray.length - 1; i >= 0; i--) {
-                var interaction = interactionsArray[i];
-                if (!interaction.getActive()) {
-                    continue;
-                }
-                var cont = interaction.handleEvent(mapBrowserEvent);
-                if (!cont) {
-                    break;
-                }
-            }
-        }
-    };
-}
+export function bootstrap() { }

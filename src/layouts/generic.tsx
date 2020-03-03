ca0ea03b40dfa603c41b0f1d5336992a62061322
Jackie Ng <jumpinjackie@gmail.com>
2020-03-03 16:25:06 +1100

    Flesh out test generic template some more

--- src/layouts/generic.tsx
+++ src/layouts/generic.tsx
@@ -5,15 +5,37 @@ import ModalLauncher from '../containers/modal-launcher';
 import FlyoutRegionContainer from '../containers/flyout-region';
 import InitWarningDisplay from '../containers/init-warning-display';
 import { useCommonTemplateState } from './hooks';
+import { ButtonGroup, Button, Intent } from '@blueprintjs/core';
+import { useDispatch } from 'react-redux';
+import { ICommand, ActiveMapTool } from '../api/common';
+import { invokeCommand, setActiveTool } from '../actions/map';
+import { getCommand, DefaultCommands } from '../api/registry/command';
+import { useViewerActiveTool } from '../containers/hooks';
 
 const GenericLayout = () => {
     const {
         locale,
-        capabilities,
     } = useCommonTemplateState();
+    const dispatch = useDispatch();
+    const activeTool = useViewerActiveTool();
+    const invokeCommandAction = (cmd: ICommand | undefined, parameters?: any) => {
+        if (cmd) {
+            dispatch(invokeCommand(cmd, parameters));
+        }
+    };
+    const zoomIn = () => invokeCommandAction(getCommand(DefaultCommands.ZoomIn));
+    const zoomOut = () => invokeCommandAction(getCommand(DefaultCommands.ZoomOut));
+    const zoomExtents = () => invokeCommandAction(getCommand(DefaultCommands.ZoomExtents));
+    const select = () => dispatch(setActiveTool(ActiveMapTool.WmsQueryFeatures));
     return <div style={{ width: "100%", height: "100%" }}>
         <PlaceholderComponent id={DefaultComponentNames.Map} locale={locale} />
         <PlaceholderComponent id={DefaultComponentNames.Navigator} locale={locale} />
+        <ButtonGroup vertical style={{ position: "absolute", left: 30, top: 30 }}>
+            <Button icon="plus" onClick={zoomIn} />
+            <Button icon="minus" onClick={zoomOut} />
+            <Button icon="zoom-to-fit" onClick={zoomExtents} />
+            <Button icon="select" intent={activeTool == ActiveMapTool.WmsQueryFeatures ? Intent.PRIMARY : Intent.NONE} onClick={select} />
+        </ButtonGroup>
         <ViewerApiShim />
         <ModalLauncher />
         <FlyoutRegionContainer />

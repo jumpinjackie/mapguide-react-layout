37cc1210fae4acb24ff02eca5d26eb2e83a9998a
Jackie Ng <jumpinjackie@gmail.com>
2021-09-18 02:01:27 +1000

    Flesh out selection panel tests

--- test/components/selection-panel.spec.tsx
+++ test/components/selection-panel.spec.tsx
@@ -3,8 +3,10 @@ import { mount } from "enzyme";
 import { SelectionPanel } from "../../src/components/selection-panel";
 import { SelectedFeatureSet } from "../../src/api/contracts/query";
 import { tr } from "../../src/api/i18n";
-import { createSelectionSet } from "../../test-data";
+import { createClientSelectionSet, createSelectionSet } from "../../test-data";
 import { CompositeSelection } from "../../src/api/composite-selection";
+import { ClientSelectionSet } from "../../src/api/contracts/common";
+import { strReplaceAll } from "../../src/utils/string";
 
 describe("components/selection-panel", () => {
     it("null selection results in empty results message", () => {
@@ -22,7 +24,7 @@ describe("components/selection-panel", () => {
         expect(wrapper.find(".selection-panel-no-selection")).toHaveLength(1);
         expect(wrapper.find(".selection-panel-no-selection").text()).toBe(tr("NO_SELECTED_FEATURES"));
     });
-    it("Empty selection results in empty results message", () => {
+    it("Empty MG selection results in empty results message", () => {
         const onZoomRequest = jest.fn();
         const onShowSelectedFeature = jest.fn();
         const set: SelectedFeatureSet = {
@@ -31,6 +33,33 @@ describe("components/selection-panel", () => {
         const cleanHTML = jest.fn();
         const allowHTMLValues = false;
         const sel = new CompositeSelection(set);
+        const wrapper = mount(<SelectionPanel cleanHTML={cleanHTML} allowHtmlValues={allowHTMLValues} selection={sel} onRequestZoomToFeature={onZoomRequest} onShowSelectedFeature={onShowSelectedFeature} />);
+        expect(wrapper.find(".selection-panel-toolbar")).toHaveLength(0);
+        expect(wrapper.find(".selection-panel-property-grid")).toHaveLength(0);
+        expect(wrapper.find(".selection-panel-no-selection")).toHaveLength(1);
+        expect(wrapper.find(".selection-panel-no-selection").text()).toBe(tr("NO_SELECTED_FEATURES"));
+    });
+    it("Empty client selection results in empty results message", () => {
+        const onZoomRequest = jest.fn();
+        const onShowSelectedFeature = jest.fn();
+        const cleanHTML = jest.fn();
+        const allowHTMLValues = false;
+        const csel: ClientSelectionSet = {
+            layers: []
+        };
+        const sel = new CompositeSelection(undefined, csel);
+        const wrapper = mount(<SelectionPanel cleanHTML={cleanHTML} allowHtmlValues={allowHTMLValues} selection={sel} onRequestZoomToFeature={onZoomRequest} onShowSelectedFeature={onShowSelectedFeature} />);
+        expect(wrapper.find(".selection-panel-toolbar")).toHaveLength(0);
+        expect(wrapper.find(".selection-panel-property-grid")).toHaveLength(0);
+        expect(wrapper.find(".selection-panel-no-selection")).toHaveLength(1);
+        expect(wrapper.find(".selection-panel-no-selection").text()).toBe(tr("NO_SELECTED_FEATURES"));
+    });
+    it("Empty MG and client selection results in empty results message", () => {
+        const onZoomRequest = jest.fn();
+        const onShowSelectedFeature = jest.fn();
+        const cleanHTML = jest.fn();
+        const allowHTMLValues = false;
+        const sel = new CompositeSelection(undefined, undefined);
         const wrapper = mount(<SelectionPanel cleanHTML={cleanHTML} allowHtmlValues={allowHTMLValues} selection={sel} onRequestZoomToFeature={onZoomRequest} onShowSelectedFeature={onShowSelectedFeature} />);
         expect(wrapper.find(".selection-panel-toolbar")).toHaveLength(0);
         expect(wrapper.find(".selection-panel-property-grid")).toHaveLength(0);
@@ -48,6 +77,19 @@ describe("components/selection-panel", () => {
         expect(wrapper.find(".selection-panel-toolbar")).toHaveLength(1);
         expect(wrapper.find(".selection-panel-property-grid")).toHaveLength(1);
         expect(wrapper.find({ "data-property-value-for": "Name" }).text()).toBe("Foo / Feature 1");
+        expect(wrapper.find("tr")).toHaveLength(3);
+    });
+    it("HTML cleaning function is called if given", () => {
+        const onZoomRequest = jest.fn();
+        const onShowSelectedFeature = jest.fn();
+        const set: SelectedFeatureSet = createSelectionSet();
+        const cleanHTML = (html: string) => strReplaceAll(html, "Foo", "Cleaned")
+        const allowHTMLValues = true;
+        const sel = new CompositeSelection(set);
+        const wrapper = mount(<SelectionPanel cleanHTML={cleanHTML} allowHtmlValues={allowHTMLValues} selection={sel} onRequestZoomToFeature={onZoomRequest} onShowSelectedFeature={onShowSelectedFeature} />);
+        expect(wrapper.find(".selection-panel-toolbar")).toHaveLength(1);
+        expect(wrapper.find(".selection-panel-property-grid")).toHaveLength(1);
+        expect(wrapper.find({ "data-property-value-for": "Name" }).text()).toBe("Cleaned / Feature 1");
         expect(wrapper.find("tr")).toHaveLength(3);
     });
     it("Can scroll forwards/backwards on first selected layer", () => {
@@ -83,7 +125,7 @@ describe("components/selection-panel", () => {
         expect(onZoomRequest.mock.calls[0]).toHaveLength(1);
         expect(onZoomRequest.mock.calls[0][0]).toBe(set.SelectedLayer[0].Feature[0]);
     });
-    it("Switching selected layer resets feature index to 0", () => {
+    it("Switching selected layer of MG selection resets feature index to 0", () => {
         const onZoomRequest = jest.fn();
         const onShowSelectedFeature = jest.fn();
         const set: SelectedFeatureSet = createSelectionSet();
@@ -101,5 +143,24 @@ describe("components/selection-panel", () => {
         expect(wrapper.find({ "data-property-value-for": "Name" }).text()).toBe("Foo / Feature 2");
         select.simulate('change', { target: { value : 1 } })
         expect(wrapper.find({ "data-property-value-for": "Name" }).text()).toBe("Bar / Feature 1");
+    });
+    it("Switching selected layer of client selection resets feature index to 0", () => {
+        const onZoomRequest = jest.fn();
+        const onShowSelectedFeature = jest.fn();
+        const set = createClientSelectionSet();
+        const cleanHTML = jest.fn();
+        const allowHTMLValues = false;
+        const sel = new CompositeSelection(undefined, set);
+        const wrapper = mount(<SelectionPanel cleanHTML={cleanHTML} allowHtmlValues={allowHTMLValues} selection={sel} onRequestZoomToFeature={onZoomRequest} onShowSelectedFeature={onShowSelectedFeature} />);
+        expect(wrapper.find(".selection-panel-toolbar")).toHaveLength(1); //, "Has Toolbar");
+        expect(wrapper.find(".toolbar-btn")).toHaveLength(3); //, "Has Toolbar Buttons");
+        const fwd = wrapper.find(".toolbar-btn").at(1);
+        const select = wrapper.find("select");
+        expect(select).toHaveLength(1); //, "Has Layer Select");
+        expect(select.find("option")).toHaveLength(2);
+        fwd.simulate("click");
+        expect(wrapper.find({ "data-property-value-for": "name" }).text()).toBe("Foo");
+        select.simulate('change', { target: { value : 1 } })
+        expect(wrapper.find({ "data-property-value-for": "description" }).text()).toBe("Bar");
     });
 });

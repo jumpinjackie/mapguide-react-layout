738e8968ed88d7a8a3f0bd5f28b28b1b70b3b680
Jackie Ng <jumpinjackie@gmail.com>
2025-05-23 21:46:31 +1000

    Fix some blueprint buttons missing labels

--- src/components/elements/providers/blueprint/button.tsx
+++ src/components/elements/providers/blueprint/button.tsx
@@ -4,5 +4,7 @@ import { iconName, variantToIntent } from "./utils";
 import React from "react";
 
 export const BpButton: React.FC<ButtonProps> = (props) => {
-    return <Button type={props.type} className={props.className} icon={iconName(props.icon)} loading={props.loading} onClick={props.onClick} active={props.active} title={props.title} style={props.style} minimal={props.minimal} intent={variantToIntent(props.variant)} disabled={props.disabled} />;
+    return <Button type={props.type} className={props.className} icon={iconName(props.icon)} loading={props.loading} onClick={props.onClick} active={props.active} title={props.title} style={props.style} minimal={props.minimal} intent={variantToIntent(props.variant)} disabled={props.disabled}>
+        {props.children}
+    </Button>
 }
